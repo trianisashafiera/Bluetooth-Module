531a296fe75f444aefb2c3b42f39d1d65343349b
trianisashafiera <73048845+trianisashafiera@users.noreply.github.com>
2020-12-22 20:01:56 +0700

    Add files via upload

---
+++ modul_bluetooth.ino
@@ -0,0 +1,23 @@
+int ml = A0;
+int h = 255;
+int l = 0;
+String motion;
+
+void setup (){
+  Serial.begin(9600);
+  pinMode (ml, OUTPUT);
+}
+
+void loop (){
+  Serial.println ("Silakan ketik 'p' untuk memutar 's' untuk berhenti");
+  while (Serial.available()==0) {   }
+
+  motion = Serial.readString();
+  if (motion == "p") {
+    analogWrite (ml,l);
+  }
+  if (motion == "s") {
+    analogWrite (ml,h);
+  }
+}
+
